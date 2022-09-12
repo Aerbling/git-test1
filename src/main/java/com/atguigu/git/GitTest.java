f8bf43b6786f3d1378606a3a4578bdcd76f78f4b
root <root@qq.com>
2022-09-12 09:08:19 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author: fg
+ * @Time: 2022-09-12 8:55
+ * @function:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
